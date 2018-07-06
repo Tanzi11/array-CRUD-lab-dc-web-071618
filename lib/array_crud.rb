1e284de79f208dc85f08a84696ae1a5bbd1ce549
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-06 20:04:29 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,9 +34,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow",1,"am",2,3,4,"arrays!"]
+  index_number = array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow",1,"am",2,3,4,"arrays!"]
+  index_number = array[-1]
 end
